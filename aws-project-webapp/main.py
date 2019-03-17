b7aa6566df4adc91772c3bb062b7d0bf030d61e7
Ubuntu <ubuntu@ip-172-31-6-60.us-west-1.compute.internal>
2019-03-17 01:12:40 +0000

    create instance when this end point is accessed

--- aws-project-webapp/main.py
+++ aws-project-webapp/main.py
@@ -4,12 +4,25 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from joblib import load
+import boto3
 
 app = Flask(__name__)
 
-@app.route('/isAlive', methods=['GET'])
-def index():
-    return jsonify(return_value = "channa works on aws project")
+def _create_instance():
+    ec2 = boto3.resource('ec2')
+    instance = ec2.create_instances(
+        ImageId = 'ami-0e355297545de2f82',
+        MinCount = 1,
+        MaxCount = 1,
+        InstanceType = 't2.micro',
+        KeyName = 'aws-project-ec2-keypair', 
+    )
+    return instance[0].id
+
+@app.route('/', methods=['GET'])
+def main_response():
+    instance_id = _create_instance()
+    return jsonify(return_value = instance_id) 
 
 if __name__ == "__main__":
     print("Loading!")
